4b24e71a4d4b26b9c24ef50396f72752dc510e5a
Kajalkansal30 <144597783+Kajalkansal30@users.noreply.github.com>
2025-05-13 20:12:53 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -173,7 +173,9 @@ export const login = async (req, res) => {
         return res.status(200).cookie("token", token, {
             maxAge: 1 * 24 * 60 * 60 * 1000,
             httpOnly: true,
-            sameSite: 'strict'
+            secure: true,
+            sameSite: 'none',
+            path: '/',
         }).json({
             message: `Welcome back, ${user.firstname} ${user.lastname}`,
             user,
